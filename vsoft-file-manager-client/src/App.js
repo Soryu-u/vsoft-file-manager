5d644913ca93ea126e1c22a749bbf78315890d74
Soryu-u <collages447@gmail.com>
2023-03-16 14:05:48 +0200

    initial client commit

---
+++ vsoft-file-manager-client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      zxc
+    </div>
+  );
+}
+
+export default App;
--- vsoft_file_manager_backend/src/users/resolvers/user/user.resolver.ts
+++ vsoft_file_manager_backend/src/users/resolvers/user/user.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { CreateUserInput } from 'src/users/inputs/create-user.input';
+import { UserService } from 'src/users/services/user/user.service';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+    constructor(
+        private readonly userService: UserService,
+    ) {
+    }
+
+    @Mutation(() => UserEntity)
+    async createUser(@Args('createUser') createUserInput: CreateUserInput): Promise<UserEntity> {
+        return await this.userService.createUser(createUserInput);
+    }
+
+    @Query(() => UserEntity)
+    async getOneUser(@Args('id') id: number): Promise<UserEntity> {
+        return await this.userService.getOneUser(id);
+    }
+
+    @Query(() => [UserEntity])
+    async getAllUser(): Promise<UserEntity[]> {
+        return await this.userService.getAllUsers();
+    }
+}
